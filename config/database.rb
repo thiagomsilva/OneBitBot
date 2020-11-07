a19e9baa8571a488400a37b7a22665187d1230c7
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2020-11-07 12:56:59 -0300

    add config database production

--- config/database.rb
+++ config/database.rb
@@ -21,3 +21,16 @@ configure :test do
       host: 'postgres'
     }
   end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+  
+    set :database, {
+      adapter:  'postgresql',
+      host:     db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
